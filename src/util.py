4e4848d7f6198dad403c982ad299a4f54b503238
Alyona <alena.gnevusheva@lightspeedhq.com>
2024-12-01 19:03:10 +0100

    setup the script to file the solution

---
+++ src/util.py
@@ -0,0 +1,55 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
+
+
+SESSION_COOKIE = os.environ['SESSION_COOKIE']
+print(SESSION_COOKIE)
+
+AOC_COOKIE = SESSION_COOKIE
+YEAR = '2024'
+
+def get_input(day):
+    req = requests.get(f'https://adventofcode.com/{YEAR}/day/{day}/input', 
+                       headers={'cookie':'session='+AOC_COOKIE})
+    return req.text
+
+def get_example(day,offset=0):
+    req = requests.get(f'https://adventofcode.com/{YEAR}/day/{day}',
+                       headers={'cookie':'session='+AOC_COOKIE})
+    return req.text.split('<pre><code>')[offset+1].split('</code></pre>')[0]
+
+def submit(day, level, answer):
+    print(f'You are about to submit the follwing answer:')
+    print(f'>>>>>>>>>>>>>>>>> {answer}')
+    input('Press enter to continue or Ctrl+C to abort.')
+    data = {
+      'level': str(level),
+      'answer': str(answer)
+    }
+
+    response = requests.post(f'https://adventofcode.com/{YEAR}/day/{day}/answer',
+                             headers={'cookie':'session='+AOC_COOKIE}, data=data)
+    if 'You gave an answer too recently' in response.text:
+        print('VERDICT : TOO MANY REQUESTS')
+    elif 'not the right answer' in response.text:
+        if 'too low' in response.text:
+            print('VERDICT : WRONG (TOO LOW)')
+        elif 'too high' in response.text:
+            print('VERDICT : WRONG (TOO HIGH)')
+        else:
+            print('VERDICT : WRONG (UNKNOWN)')
+    elif 'seem to be solving the right level.' in response.text:
+        print('VERDICT : ALREADY SOLVED')
+    else:
+        print('VERDICT : OK !')
+
+DAY = 25
+PART = 1
+s = get_input(DAY).strip()
+
+ans = 123
+
+submit(DAY, PART, ans)
