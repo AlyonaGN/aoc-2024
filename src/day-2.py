7999e9575c58425891cecdea5b1a3223737e6d70
Alyona <alena.gnevusheva@lightspeedhq.com>
2024-12-02 19:53:31 +0100

    wip: solve part 2

--- src/day-2.py
+++ src/day-2.py
@@ -1,34 +1,47 @@
 from util import get_input, submit
 from enum import Enum
 
-def calculateSafeReports(input: str) -> int:
-    Mode = Enum('MODE', [('INCREASING', 1), ('DECREASING', 2)])
+Mode = Enum('MODE', [('INCREASING', 1), ('DECREASING', 2)])
 
-    def getDiff(a: int, b: int) -> int:
+CountingRegime = Enum('COUNTING_REGIME', [('STRICT', 1), ('RELAXED', 2)])
+
+def getDiff(a: int, b: int) -> int:
         return a - b
     
-    def isDiffSafe(diff: int) -> bool:
+def isDiffSafe(diff: int) -> bool:
         absDiff = abs(diff)
         return absDiff >= 1 and absDiff <= 3
     
-    def getDiffMode(diff: int) -> bool:
+def getDiffMode(diff: int) -> bool:
         if diff < 0:
             return Mode.DECREASING
         else:
             return Mode.INCREASING
-        
+
+def calculateSafeReports(input: str, regime: CountingRegime) -> int:   
     numOfSafeReports = 0
     grid = input.splitlines()
 
     for row in grid:
         rowMode: Mode
         cells = row.split(' ')
+        canCheatABit: bool
+        distanceToPrevCell = 1
+
+        if regime == CountingRegime.STRICT:
+            canCheatABit = False
+        else:
+            canCheatABit = True
 
         for y, cell in enumerate(cells):
             if y == 0:
                 continue
             else:
-                prevCell = cells[y - 1]
+                prevCell = cells[y - distanceToPrevCell]
+                
+                if distanceToPrevCell == 2:
+                    distanceToPrevCell = 1
+
                 diff = getDiff(int(cell), int(prevCell))
                 mode = getDiffMode(diff)
 
@@ -37,9 +50,17 @@ def calculateSafeReports(input: str) -> int:
                         rowMode = getDiffMode(diff)
                     else: 
                         if mode != rowMode:
-                            break
+                            if canCheatABit:
+                                canCheatABit = False
+                                distanceToPrevCell = 2
+                            else:
+                                break
                 else:
-                    break
+                    if canCheatABit:
+                        canCheatABit = False
+                        distanceToPrevCell = 2
+                    else:
+                        break
 
                 if y == len(cells) - 1:
                     numOfSafeReports += 1
@@ -47,6 +68,7 @@ def calculateSafeReports(input: str) -> int:
     return numOfSafeReports
 
 input = get_input(2).strip()
-numOfSafeReports = calculateSafeReports(input)
-print(numOfSafeReports)
-# submit(2, 1, numOfSafeReports)
+numOfSafeReports = calculateSafeReports(input, CountingRegime.STRICT)
+numberOfSafeReportsRelaxedRegime = calculateSafeReports(input, CountingRegime.RELAXED)
+print(numberOfSafeReportsRelaxedRegime)
+# submit(2, 2, numberOfSafeReportsRelaxedRegime)
