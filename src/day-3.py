51459d399c51c54e79dd3a5a93d905518fcaa478
Alyona <alena.gnevusheva@lightspeedhq.com>
2024-12-07 17:01:14 +0100

    day 3, part 2 WIP

--- src/day-3.py
+++ src/day-3.py
@@ -12,6 +12,19 @@ def multiply(input: str) -> int:
 
 input = get_input(3).strip()
 res = multiply(input)
-print(res)
 
-submit(3, 1, res)
+# submit(3, 1, res)
+
+def multiplyWithInstructions(input: str) -> int:
+    # try negative lookbehinds 
+    # to-do: adjust for "do"
+    muls = re.finditer(r"?<!\bdon't\b[a-z])mul\(\d\d?\d?,\d\d?\d?\)", input, re.IGNORECASE)
+    total = 0
+    for mul in muls:
+        [intA, intB] = re.findall(r"\d\d?\d?", mul.group())
+        total += int(intA) * int(intB)
+
+    return total
+
+res2 = multiplyWithInstructions(input)
+print(res2)
