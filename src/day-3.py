afd18d748e11202ff29b3e344268eef5c02498c8
Alyona <alena.gnevusheva@lightspeedhq.com>
2024-12-08 12:57:32 +0100

    iterate over all matches

--- src/day-3.py
+++ src/day-3.py
@@ -19,11 +19,21 @@ def multiplyWithInstructions(input: str) -> int:
     
     mulsDosDonts = re.finditer(r"(?:mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))", input, re.IGNORECASE)
     total = 0
-    for mul in muls:
-        [intA, intB] = re.findall(r"\d\d?\d?", mul.group())
-        total += int(intA) * int(intB)
+    canDo = True
+    for m in mulsDosDonts:
+        match = m.group()
+        if (match.startswith("mul(")):
+            if canDo:
+                [intA, intB] = re.findall(r"\d{1,3}", match)
+                total += int(intA) * int(intB)
+        elif (match.startswith("do()")):
+            canDo = True
+        elif (match.startswith("don")):
+            print("yo")
+            canDo = False
 
     return total
 
 res2 = multiplyWithInstructions(input)
 print(res2)
+submit(3, 2, res)
